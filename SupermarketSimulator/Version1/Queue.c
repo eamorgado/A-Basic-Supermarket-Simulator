cbf12e0eb27845609e55c97127818156189ac2bb
eamorgado <edu.dado99@gmail.com>
2019-05-15 16:41:14 +0100

    Finished Queue

---
+++ SupermarketSimulator/Version1/Queue.c
@@ -0,0 +1,93 @@
+/*******************************************************************************
+| Program: An Implementation of a C Queue                                      |
+| Last Updated: 15/5/2019      FCUP       Copyright (c) 2019, Eduardo Morgado  |
+*******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include "Queue.h"
+
+/*------------------------------------------------------------------------------
+|                           Functions Implementation                           |
+------------------------------------------------------------------------------*/
+Queue* createQueue(){
+   /*
+    * Allocate the space in memory for the queue, initializes pointers 
+    * Returns the queue
+    */
+   Queue* Q=(Queue*)malloc(sizeof(Queue));
+   if(!Q){
+       printf("Error creating Queue\n");
+       exit(0);
+   }
+   FIRST(Q)=NULL;
+   LAST(Q)=NULL;
+   SIZE(Q)=0;
+   return Q;
+}
+int isEmpty(Queue* Q){
+    return FIRST(Q)==NULL;
+}
+int getFirst(Queue* Q){
+    return DATA(FIRST(Q));
+}
+int getLast(Queue* Q){
+    return DATA(LAST(Q));    
+}
+void enqueue(int elem, Queue* Q){
+    QueueElem* q_e=(QueueElem*)malloc(sizeof(QueueElem));
+    if(!q_e){
+        printf("Error enqueuing element %d\n",elem);
+        freeQueue(Q);
+        exit(0);
+    }
+    if(FIRST(Q)==NULL) //Queue is empty
+        FIRST(Q)=LAST(Q)=q_e;
+    else{
+        NEXT(LAST(Q))=q_e;
+        LAST(Q)=q_e;
+    }
+    NEXT(q_e)=NULL;
+    DATA(q_e)=elem;
+    SIZE(Q)++;
+}
+int dequeue(Queue* Q){
+    int data=getFirst(Q);
+    QueueElem* aux=FIRST(Q);
+    FIRST(Q)=NEXT(FIRST(Q));
+    free(aux);
+    SIZE(Q)--;
+    return data;
+}
+void printQueue(Queue* Q){
+    if(isEmpty(Q)){
+        printf("<<\n");
+        return;
+    }
+    QueueElem* curr=FIRST(Q);
+    int i=0;
+    printf("<");
+    while(NEXT(curr)!=NULL){
+        printf("[%d:%d]<",i,DATA(curr));
+        i++;
+        curr=NEXT(curr); 
+    }
+    printf("[%d:%d]<\n",(++i),DATA(curr));
+}
+void freeQueue(Queue* Q){
+    if(isEmpty(Q)){
+        free(Q);
+        return;
+    }
+    freeQueueElem(NULL,FIRST(Q));
+    free(Q);
+    return;
+}
+void freeQueueElem(QueueElem* prev,QueueElem*curr){
+    if(NEXT(curr)==NULL){
+        free(curr);
+        NEXT(prev)=NULL;
+        return;
+    }
+    freeQueueElem(curr,NEXT(curr));
+}
+
---
+++ SupermarketSimulator/Version1/Queue.h
@@ -0,0 +1,55 @@
+/*******************************************************************************
+| Program: An Definition of a C Queue                                          |
+| Last Updated: 15/5/2019      FCUP       Copyright (c) 2019, Eduardo Morgado  |
+********************************************************************************
+|   Representation:                                                            |
+*******************************************************************************/
+#ifndef QUEUE_H
+#define QUEUE_H
+
+/*------------------------------------------------------------------------------
+|                           Macro Definition                                   |
+------------------------------------------------------------------------------*/
+#define DATA(QE) ((QE)->data)
+#define NEXT(QE) ((QE)->next)
+
+#define FIRST(Q) ((Q)->first)
+#define LAST(Q) ((Q)->last)
+#define SIZE(Q) ((Q)->size)
+
+
+/*------------------------------------------------------------------------------
+|                           Queue Element                                      |
+------------------------------------------------------------------------------*/
+typedef struct queuelement{
+    int data;
+    struct queuelement* next;
+}QueueElem;
+
+
+/*------------------------------------------------------------------------------
+|                           Queue Definition                                   |
+------------------------------------------------------------------------------*/
+typedef struct queue{
+    QueueElem* first;
+    QueueElem* last;
+    int size;
+}Queue;
+
+
+/*------------------------------------------------------------------------------
+|                           Queue Functions                                    |
+------------------------------------------------------------------------------*/
+Queue* createQueue();
+int isEmpty(Queue*);
+int getFirst(Queue*);
+int getLast(Queue*);
+void enqueue(int,Queue*);
+int dequeue(Queue*);
+void printQueue(Queue*);
+
+/*------------------------------Free------------------------------------------*/
+void freeQueue(Queue*);
+void freeQueueElem(QueueElem*,QueueElem*);
+
+#endif
