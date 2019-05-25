3b90978c5d173be83e67f8ae96634581805b4714
eamorgado <edu.dado99@gmail.com>
2019-05-26 00:37:51 +0100

    Update

--- PriorityQueue/PriorityQueue.h
+++ PriorityQueue/PriorityQueue.h
@@ -1,21 +1,35 @@
+/*******************************************************************************
+| Program: A Definition of a C Priority Queue of Integers                      |
+| Last Updated: 17/5/2019                                                  FCUP|
+*******************************************************************************/
 #include "Queue.h"
 #ifndef PRIORITY_QUEUE_H
 #define PRIORITY_QUEUE_H
 
+/*------------------------------------------------------------------------------
+|                           Macro Definition                                   |
+------------------------------------------------------------------------------*/
 #define URGENT(PQ) ((PQ)->urgent)
 #define NORMAL(PQ) ((PQ)->normal)
 
 
+/*------------------------------------------------------------------------------
+|                           PQueue Definition                                  |
+------------------------------------------------------------------------------*/
 typedef struct priorityqueue{
     Queue* urgent;
     Queue* normal;
 }PriorityQueue;
 
-PriorityQueue* newPriorityQueue();
-void addElem(int,int,PriorityQueue*);
 
-void raiseUrgency(PriorityQueue*);
-int serviceElement(PriorityQueue*);
+/*------------------------------------------------------------------------------
+|                           PQueue Functions                                   |
+------------------------------------------------------------------------------*/
+PriorityQueue* newPriorityQueue();
+void addElem(int,int,PriorityQueue*); //enqueue in queue of priority int
+
+void raiseUrgency(PriorityQueue*); //Increases priority of first element in Normal
+int serviceElement(PriorityQueue*); //Gets element with highest priority
 void printPriorityQueue(PriorityQueue*);
 
 #endif
